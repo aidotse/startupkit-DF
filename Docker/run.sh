d6b417e98a31ddee7b859e8307926ca977fa0898
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-23 15:33:31 +0200

    Add header

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# File              : run.sh
+# Author            : Sheetal Reddy <sheetal.reddy@ai.se>
+# Date              : 23.10.2020
+# Last Modified Date: 23.10.2020
+# Last Modified By  : Sheetal Reddy <sheetal.reddy@ai.se>
 
 ROOT_DIR=$PWD/..
 DATA_DIR=$ROOT_DIR/data
